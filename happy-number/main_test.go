544872a240a2bcada48e0a6dc62a50b112a70620
Adam Shelton <adamshelton@rti.org>
2022-03-30 17:47:53 -0400

    simplify

--- happy-number/main_test.go
+++ happy-number/main_test.go
@@ -4,7 +4,6 @@ import "testing"
 
 func FuzzIsHappy(f *testing.F) {
 	f.Add(19, true)
-	f.Add(2, false)
 	f.Fuzz(func(t *testing.T, num int, want bool) {
 		if got := isHappy(num); got != want {
 			t.Fatalf("got %t , want %t for num %d", got, want, num)
